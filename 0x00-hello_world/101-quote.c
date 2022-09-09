d6a3a8bea4ca7a750a360d91ecf1c96232f535a8
Blazey-B <ibrado97@gmail.com>
2022-09-09 12:56:28 -0700

    number 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,15 +1,14 @@
-#include <stdio.h>
-
+#include <unistd.h>
 /**
  * main - the entry point of the program
  *
  * Description - 101-quote.c
  *
- * Return: zero if no error, Non-zero value if errors.
+ * Return: Always 1 (Success)
  */
 int main(void)
 {
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n".);
+	char str1[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n"; write (2, str1. 59);
 		return (1);
 }
 
