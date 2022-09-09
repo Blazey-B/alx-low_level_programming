d3fdd6a8835f7753b8edfed021e756e7c1e89f45
Blazey-B <ibrado97@gmail.com>
2022-09-09 12:04:22 -0700

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - the entry point of the program
+ *
+ * Description - 101-quote.c
+ *
+ * Return: zero if no error, Non-zero value if errors.
+ */
+int main(void)
+{
+	fwrite("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+		return (0);
+}
+
+
