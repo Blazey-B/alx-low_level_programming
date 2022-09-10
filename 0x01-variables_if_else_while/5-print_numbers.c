b99bb4fa3d268cd57933732b0c6289bbc31752bd
Blazey-B <ibrado97@gmail.com>
2022-09-09 19:31:31 -0700

    5 now

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -2,7 +2,7 @@
 /**
  * main - printing numbers from 0-9 with commas and space between them
  * Description: using the main function
- * this program prints "0, 1, 2, 3, 4, 5, 6, 7, 8, 9"
+ * this program prints "0123456789"
  * Return: 0
  */
 int main(void)
@@ -14,7 +14,6 @@ int main(void)
 		putchar(c);
 		if (c != 57)
 		{
-			putchar(',');
 			putchar(' ');
 		}
 	}
