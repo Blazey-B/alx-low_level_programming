481570b8bf81f2fb7700cd00607c99b2e63b6106
Blazey-B <ibrado97@gmail.com>
2022-09-13 12:36:09 -0700

    lets try this

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,11 +1,11 @@
-#include <stdio.h>
+#include <main.h>
 
 /**
  * main - Prints the alphabet in lowercase.
  *
  * Return: Always 0.
  */
-int main(void)
+void print_alphabet(void)
 {
 	char letter;
 
@@ -13,6 +13,6 @@ int main(void)
 		putchar(letter);
 
 	putchar('\n');
-	void print_alphabet(void);
+	
 	return (0);
 }
