cd6d8dcfc13860845d629f3ea26924bfadcfd3aa
Blazey-B <ibrado97@gmail.com>
2022-09-19 19:02:12 -0700

    1,2&3

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,8 +1,8 @@
 #include "main.h"
 
-/** Main is the entry point of the program
- * A function that takes a pointer to an int as parameter and updates the value it points to to 98.
- * Return: Always 0
+/** Main: Entry point fo the program
+ * Description: a function that takes a pointer to an int, updates value to 98
+ * Return: Always 0.
  */
 
 void reset_to_98(int *n)
---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * Main: Entry point
+ * Desc: Function to return the length of a string
+ * Return: Always 0.
+ */
+
+int _strlen(char *s)
+{
+	while (*str)
+		_putchar(*str++);
+
+	_putchar('\n');
+}
