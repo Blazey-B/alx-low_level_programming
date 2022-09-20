b709e572cf2bb1209d139ddf022c51261fa4e8e1
Blazey-B <ibrado97@gmail.com>
2022-09-19 18:32:16 -0700

    trial 03

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /** Main is the entry point of the program
- * 0-reset_to_98.c
+ * A function that takes a pointer to an int as parameter and updates the value it points to to 98.
  * Return: Always 0
  */
 
