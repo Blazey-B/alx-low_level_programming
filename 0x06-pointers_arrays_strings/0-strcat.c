19cdaa2c02cd320822d17c3b9ded119a99123ebc
Blazey-B <ibrado97@gmail.com>
2022-09-22 15:41:35 -0700

    myCommit

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * A function that concatenates two strings
+ *
+ * Return: (0)
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	/* concatenates src to dest */
+	strcat(src, dest)
+
+		result 0;
+}
--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -2,7 +2,7 @@
 #define MAIN_H
 
 /* Prototype list */
-_putchar();
+void _putchar();
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
