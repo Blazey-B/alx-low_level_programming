7f239685c4460b1bcf25ce90fedeb194fd7a5b92
Blazey-B <ibrado97@gmail.com>
2022-10-06 20:55:52 -0700

    4th trial

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -13,6 +13,5 @@ char *create_xarray(int size);
 char *iterate_zeroes(char *str);
 void get_prod(char *prod, char *mult, int digit, int zeroes);
 void add_nums(char *final_prod, char *next_prod, int next_len);
-void _putchar();
-
+int _putchar(char c);
 #endif
