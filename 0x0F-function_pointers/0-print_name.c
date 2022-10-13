2c850faa4b383093d08b058cbc8eda76c50f3873
Blacc <111024886+Blazey-B@users.noreply.github.com>
2022-10-12 21:48:39 -0700

    retrying 0

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,5 +1,5 @@
 #include "function_pointers.h"
-#include <stdio.h>
+#include <stdlib.h>
 
 /**
  * print_name - Prints a name.
@@ -8,8 +8,8 @@
  */
 void print_name(char *name, void (*f)(char *))
 {
-	if (name == NULL || f == NULL)
-		return;
-
+	if (f != NULL)
 	f(name);
+
+
 }
